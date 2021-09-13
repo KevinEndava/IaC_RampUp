92e6cc827c32cbd9aaca3aca7444e6010f6193bd
Kevin Puentes [Intern] <Kevin.Puentes@endava.com>
2021-09-13 17:41:10 -0500

    Hotfix

--- network.tf
+++ network.tf
@@ -18,6 +18,6 @@ resource "google_compute_subnetwork" "kubernetes-subnet-rampup2" {
   project = var.project  
   name          = "kubernetes-subnet-${var.project_type}"
   ip_cidr_range = "10.0.2.0/24"
-  network       = google_compute_network.vpc-var.project_type.id
+  network       = google_compute_network.vpc-rampup2.id
   region = var.region
 }
