5f51ae28de4d1d944dfaa67240b86bd6282eee41
Kevin Puentes [Intern] <Kevin.Puentes@endava.com>
2021-09-14 14:58:17 -0500

    New Firewall

--- firewalls.tf
+++ firewalls.tf
@@ -1,12 +1,12 @@
-# resource "google_compute_firewall" "rules" {
-#   project     = "kubernetes-ramp-up"
-#   name        = "public-firewall-${var.project_type}"
-#   network     = "vpc-${var.project_type}"
-#   description = "Creates firewall rule targeting tagged instances"
+resource "google_compute_firewall" "rules" {
+  project     = "kubernetes-ramp-up"
+  name        = "public-firewall-${var.project_type}"
+  network     = "vpc-${var.project_type}"
+  description = "Creates firewall rule targeting tagged instances"
 
-#   allow {
-#     protocol  = "tcp"
-#     ports     = ["80", "8080", "1000-2000"]
-#   }
-#   target_tags = ["web"]
-# }
+  allow {
+    protocol  = "tcp"
+    ports     = ["80", "8080", "1000-2000"]
+  }
+  target_tags = ["public"]
+}
