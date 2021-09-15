f061a559001701f9a3c30a287def43e82a7006e2
Kevin Puentes [Intern] <Kevin.Puentes@endava.com>
2021-09-15 09:48:35 -0500

    New Instance

--- instances.tf
+++ instances.tf
@@ -1,6 +1,6 @@
 
 resource "google_compute_instance" "default" {
-  name         = "CICD_Jumpbox"
+  name         = "cicd-jumpbox-rampup2"
   machine_type = "e2-small"
   zone         = "us-central1-a"
   project = var.project
