e71463056f5ef197dfbe3caa2230457c98b8b2c6
Kevin Puentes [Intern] <Kevin.Puentes@endava.com>
2021-09-16 17:00:19 -0500

    New Instance

--- instances.tf
+++ instances.tf
@@ -2,7 +2,7 @@
 resource "google_compute_instance" "default" {
   name         = "cicd-jumpbox-rampup2"
   machine_type = "e2-small"
-  zone         = "us-central1-a"
+  zone         = var.zone
   project = var.project
   tags = ["public"]
 
@@ -25,3 +25,59 @@ resource "google_compute_instance" "default" {
   }
    
 }
+
+
+resource "google_compute_instance_template" "instance_template-rampup2" {
+  name_prefix  = "instance-template-rampup2"
+  machine_type = "e2-small"
+  region       = var.region
+
+  // boot disk
+  disk {
+    boot = "ubuntu-2004-focal-v20210908"
+  }
+
+  // networking
+  network_interface {
+    network = "vpc-${var.project_type}"
+    subnetwork = "kubernetes-subnet-${var.project_type}"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# resource "google_compute_instance_group_manager" "master-node-instance_group_manager" {
+#   name               = "master-node-instance-group-rampup2"
+#   instance_template  = google_compute_instance_template.instance_template-rampup2.id
+#   base_instance_name = "instance-group-manager"
+#   zone               = var.zone
+#   target_size        = "1"
+# }
+
+# resource "google_compute_instance_group" "worker-node-instance-group-rampup2" {
+#   name        = "worker-node-instance-group-${var.project_type}"
+#   description = "Worker node instance group"
+
+#   instances = [
+#     google_compute_instance.test.id,
+#     google_compute_instance.test2.id,
+#   ]
+
+#   named_port {
+#     name = "http"
+#     port = "8080"
+#   }
+
+#   named_port {
+#     name = "https"
+#     port = "8443"
+#   }
+
+#   zone = "us-central1-a"
+# }
