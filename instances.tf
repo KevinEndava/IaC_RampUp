8d83aea139405925f87fb1f45bc9705b0668c396
Kevin Puentes [Intern] <Kevin.Puentes@endava.com>
2021-09-17 11:04:29 -0500

    New Instance Template

--- instances.tf
+++ instances.tf
@@ -1,30 +1,30 @@
 
-# resource "google_compute_instance" "default" {
-#   name         = "cicd-jumpbox-rampup2"
-#   machine_type = "e2-small"
-#   zone         = var.zone
-#   project = var.project
-#   tags = ["public"]
+resource "google_compute_instance" "default" {
+  name         = "cicd-jumpbox-rampup2"
+  machine_type = "e2-small"
+  zone         = var.zone
+  project = var.project
+  tags = ["public"]
 
-#   boot_disk {
-#     initialize_params {
-#       image = "ubuntu-2004-focal-v20210908"
-#     }
-#   }
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-2004-focal-v20210908"
+    }
+  }
 
-#   allow_stopping_for_update = false
+  allow_stopping_for_update = false
 
 
-#   network_interface {
-#     network = "vpc-${var.project_type}"
-#     subnetwork = "management-subnet-${var.project_type}"
+  network_interface {
+    network = "vpc-${var.project_type}"
+    subnetwork = "management-subnet-${var.project_type}"
 
-#     access_config {
-#       // Ephemeral public IP
-#     }
-#   }
+    access_config {
+      // Ephemeral public IP
+    }
+  }
    
-# }
+}
 
 
 # resource "google_compute_instance_template" "instance_template-rampup2" {
