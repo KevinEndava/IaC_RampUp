b60877faac4119c0a58efddc547259a954dd72a8
Kevin Puentes [Intern] <Kevin.Puentes@endava.com>
2021-09-20 15:38:39 -0500

    New Firewall

--- firewalls.tf
+++ firewalls.tf
@@ -16,7 +16,7 @@ resource "google_compute_firewall" "fw-allow-lb-rampup2" {
   name        = "fw-allow-lb-access"
   network     = "vpc-network-${var.project_type}"
   description = "Creates firewall to allow traffic from sources in the 10.0.1.0/24 and 10.0.2.0/24 ranges. This rule allows traffic from any client located in either of the two subnets."
-  source_ranges = [ "10.0.1.0/24, 10.0.2.0/24" ]
+  source_ranges = [ "10.0.1.0/24", "10.0.2.0/24" ]
   allow {
     protocol  = "tcp"
     ports     = ["22","80", "8080", "1000-2000"]
